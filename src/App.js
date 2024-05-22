b68505c83d4080b934d491b6d275fbd1823a8f43
Inna Huzhva <guzhva.inna@gmail.com>
2024-05-22 19:14:31 +0300

    Allow to switch API type

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import PriceSorting from "./panel/PriceSorting.js";
 import { useLocalStorageApi } from "./localStorageApi.js";
 import * as realApi from "./api.js";
 import "./app.scss";
+import "./slider.scss";
 
 function App() {
+  const [apiOption, setApiOption] = useState("backend");
+  const switchApi = () =>
+    setApiOption(apiOption === "backend" ? "localStorage" : "backend");
   const [roomsFilter, setRoomsFilter] = useState("any");
   const [priceSorting, setPriceSorting] = useState("asc");
   const [reload, setReload] = useState(true);
@@ -15,19 +19,27 @@ function App() {
   const [[loadingStatus, loadingError, apartments], setLoadingResult] =
     useState(["LOADING", undefined, undefined]);
   const localStorageApi = useLocalStorageApi();
+  const api = apiOption === "backend" ? realApi : localStorageApi;
 
   useEffect(() => {
     setLoadingResult(["LOADING", undefined, undefined]);
-    realApi
+    api
       .getApartments(priceSorting, roomsFilter)
       .then((apartments) =>
         setLoadingResult(["SUCCESS", undefined, apartments]),
       )
       .catch((err) => setLoadingResult(["ERROR", err.message, undefined]));
-  }, [priceSorting, roomsFilter, reload]);
+  }, [priceSorting, roomsFilter, reload, apiOption]);
 
   return (
     <div className="app">
+      <div className="api-selector">
+        <span className="type">API: {apiOption}</span>
+        <label className="switch">
+          <input type="checkbox" onChange={switchApi} />
+          <span className="slider"></span>
+        </label>
+      </div>
       <div className="header">Apartments marketplace</div>
       {loadingStatus === "LOADING" && <div className="loader">Loading....</div>}
       {loadingStatus === "ERROR" && (
@@ -38,7 +50,7 @@ function App() {
           <div>Available apartments: {apartments.length}</div>
           <div className="action-panel">
             <AddNewApartment
-              addApartment={realApi.addApartment}
+              addApartment={api.addApartment}
               refreshApartments={refresh}
             />
             <PriceSorting
@@ -52,7 +64,7 @@ function App() {
           </div>
           <ListOfApartments
             apartments={apartments}
-            deleteApartment={realApi.deleteApartment}
+            deleteApartment={api.deleteApartment}
             refreshApartments={refresh}
           />
         </>
--- src/localStorageApi.js
+++ src/localStorageApi.js
@@ -8,31 +8,40 @@ export const useLocalStorageApi = () => {
   );
 
   const getApartments = (priceSorting, roomsFilter) => {
-    const filtered =
-      roomsFilter === "any"
-        ? apartments
-        : apartments.filter((a) =>
-            roomsFilter === "4+"
-              ? a.rooms >= 4
-              : a.rooms.toString() === roomsFilter,
-          );
-    const sorted = filtered.toSorted((a, b) =>
-      priceSorting === "asc" ? a.price - b.price : b.price - a.price,
-    );
-    return sorted;
+    return new Promise((resolve) => {
+      const filtered =
+        roomsFilter === "any"
+          ? apartments
+          : apartments.filter((a) =>
+              roomsFilter === "4+"
+                ? a.rooms >= 4
+                : a.rooms.toString() === roomsFilter,
+            );
+      const sorted = filtered.toSorted((a, b) =>
+        priceSorting === "asc" ? a.price - b.price : b.price - a.price,
+      );
+      resolve(sorted);
+    });
   };
 
   const addApartment = (newApartment) => {
-    const maxId =
-      apartments.length === 0
-        ? 0
-        : Math.max(...apartments.map((a) => Number(a.id)));
-    const newId = "" + (maxId + 1);
-    setApartments([...apartments, { id: newId, ...newApartment }]);
+    return new Promise((resolve) => {
+      const maxId =
+        apartments.length === 0
+          ? 0
+          : Math.max(...apartments.map((a) => Number(a.id)));
+      const newId = "" + (maxId + 1);
+      newApartment = { id: newId, ...newApartment };
+      setApartments([...apartments, newApartment]);
+      resolve(newApartment);
+    });
   };
 
   const deleteApartment = (apartment) => {
-    setApartments(apartments.filter((a) => a.id !== apartment.id));
+    return new Promise((resolve) => {
+      setApartments(apartments.filter((a) => a.id !== apartment.id));
+      resolve();
+    });
   };
 
   return {
